067670ba36695eeeb78bcc78ef3d722d67194aec
pedrolaxe <pedrolaxe@gmail.com>
2020-01-23 11:42:19 -0300

    Version 0.4.3 - Link Supports

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
 $(document).ready(function(){
 
 	var url = 'http://www.dontpad.com/';
-	var urlcode = Math.random().toString(5).substring(2, 7) + Math.random().toString(5).substring(2, 7);
-	var urlmontada = url+urlcode;
-	
-	//$('#salvar').click(function(){
+	var ramdom = Math.random().toString(5).substring(2, 7) + Math.random().toString(5).substring(2, 7);
+	var urlcode = url+ramdom;
+
 	$('#codepad').on('change keyup', function(){
+		var linkval = $('#link').val().trim();
+		var urllink = url+linkval;
 	
 		var dpad = $('#codepad').val();
 		
 		if(dpad.length > 0 ){
+			if($('#link').val().length === 0){
+
 			$.ajax({ 
-				url: urlmontada,
+				url: urlcode,
 				data: {"text": dpad},
 				type: 'post',
 				beforeSend: function() {
 					$('#response').html('<img src="loader.gif">');
 				  },
 				success: function(result){
-				$('#saveto').text(urlmontada).show(500);
+				$('#saveto').text(urlcode).show(500);
 				$('#response').html('');
-				console.log(urlmontada);
+				console.log(urlcode);
 				}
 			});
+
+		}else{
+		
+			$.ajax({
+				url: urllink,
+				data: {"text": dpad},
+				type: 'post',
+				beforeSend: function() {
+					$('#response').html('<img src="loader.gif">');
+				  },
+				success: function(result){
+				$('#saveto').text(urllink).show(500);
+				$('#response').html('');
+				console.log(urllink);
+				}
+			});
+		}//else
 		}//if
 
-		
-	
 	
 	});
-
 });
